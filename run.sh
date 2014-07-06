c546368722d668711e1eb232be1647a84bb0029b
PouriaMaleki <dev.maleki@gmail.com>
2014-07-06 17:38:46 +0000

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(cd ~/Jik/scripts && jitter coffee js -bm) &
-(cd ~/Jik/scripts && watchify js/pg/pg.js --path js --noparse=FILE --dg false -s pg -o dist/pg.js) &
-(cd ~/Jik/styles && compass watch) &
-(cd ~/Jik && nodemon)
+(cd ~/workspace/jik/scripts && jitter coffee js -bm) &
+(cd ~/workspace/jik/scripts && watchify js/pg/pg.js --path js --noparse=FILE --dg false -s pg -o dist/pg.js) &
+(cd ~/workspace/jik/styles && compass watch) &
+(cd ~/workspace/jik && nodemon)
